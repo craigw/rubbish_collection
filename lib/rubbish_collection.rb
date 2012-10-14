f88675c1390c1efa7115ae4df7183e55a101d156
George Sheppard <george@fuzzmonkey.co.uk>
2012-10-14 21:50:42 +0100

    added collection times for Rushmoor borough council

--- lib/rubbish_collection.rb
+++ lib/rubbish_collection.rb
@@ -70,13 +70,15 @@ module RubbishCollection
     end
   end
 
-  def self.times_at_postcode postcode
+  def self.times_at_postcode postcode, house_number="1"
     times = CollectionTimes.new
     local_authority = LocalAuthority::LocalAuthority.find_by_postcode postcode
     return times if local_authority.nil?
     adaptor = adapter_for local_authority
-    adaptor.collection_times_at(postcode).each do |t|
-      times << t
+    if adaptor.method(:collection_times_at).arity == 2
+      adaptor.collection_times_at(postcode, house_number).each {|t| times << t }
+    else
+      adaptor.collection_times_at(postcode).each {|t| times << t }
     end
     times
   end
@@ -98,3 +100,4 @@ end
 
 require 'rubbish_collection/redbridge'
 require 'rubbish_collection/southwark'
+require 'rubbish_collection/rushmoor'
